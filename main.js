a08515362fb48f967c43d3e998bd31748ae0cd35
Mahdi Dibaiee <mdibaiee@aol.com>
2015-03-16 09:05:23 +0330

    Use double quotes instead of single quotes fixes #2

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-function FilterToolTip(el, value = '') {
+function FilterToolTip(el, value = "") {
   this.el = el;
   this.value = value;
 
-  let list = document.createElement('ul');
+  let list = document.createElement("ul");
   el.appendChild(list);
   el.insertBefore(list, el.firstChild);
   this.container = el;
@@ -14,37 +14,37 @@ function FilterToolTip(el, value = '') {
   this.setCSS(value);
 
   // Event Listeners
-  let select = this.filterSelect = el.querySelector('select');
+  let select = this.filterSelect = el.querySelector("select");
   this.populateFilterSelect();
 
-  let addButton = el.querySelector('#add-filter');
-  addButton.addEventListener('click', e => {
+  let addButton = el.querySelector("#add-filter");
+  addButton.addEventListener("click", e => {
     if(!select.value) return;
 
     let key = select.value;
     let def = this._definition(key);
-    let u = '';
+    let u = "";
     switch (def.unit) {
-      case 'percentage':
-        u = '%';
+      case "percentage":
+        u = "%";
         break;
-      case 'length':
-        u = 'px';
+      case "length":
+        u = "px";
         break;
-      case 'angle':
-        u = 'deg';
+      case "angle":
+        u = "deg";
         break;
-      case 'string':
-        u = '';
+      case "string":
+        u = "";
         break;
       default:
-        u = 'px';
+        u = "px";
     }
     this.add(key, def.range[0] + u);
     this.render();
   });
-  this.list.addEventListener('click', e => {
-    if(e.target.tagName.toLowerCase() === 'button') {
+  this.list.addEventListener("click", e => {
+    if(e.target.tagName.toLowerCase() === "button") {
       let key = e.target.previousElementSibling.previousElementSibling.textContent;
       this.remove(e.target.previousElementSibling.id);
       this.render();
@@ -54,15 +54,15 @@ function FilterToolTip(el, value = '') {
 
 FilterToolTip.prototype = {
   render() {
-    this.list.innerHTML = '';
-    let base = document.createElement('li');
+    this.list.innerHTML = "";
+    let base = document.createElement("li");
 
-    base.appendChild(document.createElement('label'));
+    base.appendChild(document.createElement("label"));
 
-    base.appendChild(document.createElement('input'));
+    base.appendChild(document.createElement("input"));
 
-    let removeButton = document.createElement('button');
-    removeButton.className = 'filter-editor-remove-button';
+    let removeButton = document.createElement("button");
+    removeButton.className = "filter-editor-remove-button";
     base.appendChild(removeButton);
 
     for(let [index, filter] of this.filters.entries()) {
@@ -73,24 +73,24 @@ FilterToolTip.prototype = {
       let [label, input] = el.children;
       let [min, max] = def.range;
 
-      label.className = 'filter-editor-item-label';
+      label.className = "filter-editor-item-label";
       label.textContent = filter.name;
 
-      input.classList.add('filter-editor-item-editor');
+      input.classList.add("filter-editor-item-editor");
       switch (def.unit) {
-        case 'percentage':
-        case 'angle':
-          input.type = 'range';
-          input.classList.add('devtools-rangeinput');
+        case "percentage":
+        case "angle":
+          input.type = "range";
+          input.classList.add("devtools-rangeinput");
           break;
-        case 'length':
-          input.type = 'number';
+        case "length":
+          input.type = "number";
           input.min = 0;
-          input.classList.add('devtools-textinput');
+          input.classList.add("devtools-textinput");
           break;
-        case 'string':
-          input.type = 'text';
-          input.classList.add('devtools-textinput');
+        case "string":
+          input.type = "text";
+          input.classList.add("devtools-textinput");
           break;
       }
 
@@ -100,7 +100,7 @@ FilterToolTip.prototype = {
       input.id = index;
 
       this.list.appendChild(el);
-      input.addEventListener('change', this._updateEvent.bind(this));
+      input.addEventListener("change", this._updateEvent.bind(this));
       input.focus();
     }
   },
@@ -122,13 +122,13 @@ FilterToolTip.prototype = {
     let [min, max] = filter.range;
 
     let unit = /\D+/.exec(value);
-    unit = unit ? unit[0] : '';
-    if(filter.unit !== 'string') value = parseInt(value, 10);
+    unit = unit ? unit[0] : "";
+    if(filter.unit !== "string") value = parseInt(value, 10);
 
     if(min && value < min) value = min;
     if(max && value > max) value = max;
 
-    if(filter.unit == 'string' && value == null) value = '';
+    if(filter.unit == "string" && value == null) value = "";
 
     this.filters.push({value, unit, name: filter.name});
   },
@@ -137,20 +137,20 @@ FilterToolTip.prototype = {
     if(!filter) return false;
 
     let current = this.get(name);
-    if(!current) return '';
-    return (current.value || '0') + current.unit;
+    if(!current) return "";
+    return (current.value || "0") + current.unit;
   },
   remove(id) {
     this.filters.splice(id, 1);
   },
   css(name) {
     return this.filters.map(filter => {
-      return filter.name + '(' + filter.value + filter.unit + ')';
-    }).join(' ');
+      return filter.name + "(" + filter.value + filter.unit + ")";
+    }).join(" ");
   },
   setCSS(value) {
-    value.replace(/\s{2,}/g, ' ').split(' ').forEach(a => {
-      let [key, value] = a.split('(');
+    value.replace(/\s{2,}/g, " ").split(" ").forEach(a => {
+      let [key, value] = a.split("(");
       value = value.slice(0, -1); // remove the last parantheses
       this.add(key, value);
     });    
@@ -162,7 +162,7 @@ FilterToolTip.prototype = {
   populateFilterSelect() {
     let select = this.filterSelect;
     filterList.forEach(function(filter) {
-      let option = document.createElement('option');
+      let option = document.createElement("option");
       option.innerHTML = option.value = filter.name;
       select.appendChild(option);
     });
@@ -172,7 +172,7 @@ FilterToolTip.prototype = {
 
 // Test
 
-let tp = new FilterToolTip(document.getElementById('editor'), 'blur(30px) grayscale(200%)');
+let tp = new FilterToolTip(document.getElementById("editor"), "blur(30px) grayscale(200%)");
 
 tp.render();
 
@@ -187,7 +187,7 @@ tp.render();
 
 
 
-document.getElementById('switch').addEventListener('click', e => {
+document.getElementById("switch").addEventListener("click", e => {
   var theme = document.body.className == "theme-dark" ? "theme-light" : "theme-dark";
   document.body.className = theme;
 
